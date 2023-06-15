2b3cedfed69272e59b21294e8483fbd2aef2ec59
0xNOY <noy@abc-net.jp>
2023-06-15 20:12:07 +0900

    fix: ソースギャラリーが常にプレビューモードになるように

--- main.py
+++ main.py
@@ -265,9 +265,17 @@ class PieceMaker:
 
         return tmpl_state, gr.update(value=tmpl_state.painted_img)
 
-    def add_to_queue(self, tmpl_state: TemplateFrame, queue: List[TemplateFrame]):
+    def add_to_queue(
+        self,
+        tmpl_state: TemplateFrame,
+        queue: List[TemplateFrame],
+        srcs: Tuple[List[dict], List[str]],
+    ):
         with open(self.src_tmpl_dir / f"{tmpl_state.name}{TMPL_SUFFIX}", "wb") as f:
             pickle.dump(tmpl_state, f)
+
+        i = srcs[1].index(tmpl_state.name)
+        srcs[0][i] = tmpl_state.painted_img
 
         queue.append(tmpl_state)
 
@@ -275,7 +283,7 @@ class PieceMaker:
         for tmpl_state in queue:
             queue_gallery.append((tmpl_state.painted_img, tmpl_state.name))
 
-        return (gr.update(value=None, label=None), queue, queue_gallery)
+        return (gr.update(value=None, label=None), queue, queue_gallery, srcs)
 
     def make_pieces(
         self,
@@ -559,6 +567,7 @@ class PieceMaker:
                 self.set_tmpl_img,
                 inputs=[source_gallery],
                 outputs=[img_tmpl_preview, tmpl_state, source_gallery],
+                _js="(x,y) => [x.splice(0,x.length),y=x.selectData]",
             )
 
             btn_undo_click.click(
@@ -584,8 +593,8 @@ class PieceMaker:
 
             btn_add_to_queue.click(
                 self.add_to_queue,
-                inputs=[tmpl_state, queue],
-                outputs=[img_tmpl_preview, queue, queue_gallery],
+                inputs=[tmpl_state, queue, source_gallery],
+                outputs=[img_tmpl_preview, queue, queue_gallery, source_gallery],
             )
 
             btn_clear_queue.click(
